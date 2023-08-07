989d3cc82ea63a2a67298bd688c5422fdc59ff7a
thakuratul2 <pratapsinghatul111@gmail.com>
2023-08-07 20:42:09 +0530

    Hero Done

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => {
+  return (
+    <>
+        <div className="hero-section">
+<div className="hero-message">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<a href='#'>Labore porro cumque temporibus et. Unde, alias?</a></p>
+</div>
+        </div>
+    </>
+  );
+}
+
+export default Hero;
