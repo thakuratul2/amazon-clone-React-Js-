aba189aecd3a8574b31a7e4c921f3625dea312e5
thakuratul2 <pratapsinghatul111@gmail.com>
2023-08-07 20:56:52 +0530

    Shop Start

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Hero from './Hero';
+import Shop from './Shop';
 
 const Header = () => {
   return (
     <>
         <Navbar />
         <Hero />
+        <Shop />
     </>
   );
 }
---
+++ src/components/Shop.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+  return (
+    <>
+        <div className="shop-section">
+            <div className="box1 box">
+            <div className="box-content">
+            <h2>
+                Health & Personal Care
+            </h2>
+            <div className="box-img1">
+
+            </div>
+            </div>
+            <p>See More</p>
+            </div>
+            <div className="box2 box">
+                box2
+            </div>
+            <div className="box3 box">
+                box3
+            </div>
+            <div className="box4 box">
+                box4
+            </div>
+        </div>
+    </>
+  );
+}
+
+export default Shop;
