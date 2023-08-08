203424fd31bc90e93a810af30c2cb96362a6a95e
thakuratul2 <pratapsinghatul111@gmail.com>
2023-08-08 23:19:15 +0530

    Hero

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -17,13 +17,75 @@ const Shop = () => {
             <p>See More</p>
             </div>
             <div className="box2 box">
-                box2
+            <div className="box-content">
+            <h2>
+                Electronics
+            </h2>
+            <div className="box-img2">
+
+            </div>
+            </div>
             </div>
             <div className="box3 box">
-                box3
+            <div className="box-content">
+            <h2>
+                Health & Personal Care
+            </h2>
+            <div className="box-img3">
+
+            </div>
+            </div>
             </div>
             <div className="box4 box">
-                box4
+            <div className="box-content">
+            <h2>
+                Health & Personal Care
+            </h2>
+            <div className="box-img4">
+
+            </div>
+            </div>
+            </div>
+            <div className="box1 box">
+            <div className="box-content">
+            <h2>
+                Health & Personal Care
+            </h2>
+            <div className="box-img5">
+
+            </div>
+            </div>
+            <p>See More</p>
+            </div>
+            <div className="box2 box">
+            <div className="box-content">
+            <h2>
+                
+            </h2>
+            <div className="box-img6">
+
+            </div>
+            </div>
+            </div>
+            <div className="box3 box">
+            <div className="box-content">
+            <h2>
+                Health & Personal Care
+            </h2>
+            <div className="box-img7">
+
+            </div>
+            </div>
+            </div>
+            <div className="box4 box">
+            <div className="box-content">
+            <h2>
+                Health & Personal Care
+            </h2>
+            <div className="box-img8">
+
+            </div>
+            </div>
             </div>
         </div>
     </>
