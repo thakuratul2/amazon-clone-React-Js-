0ca6050d148e4e861c9a1de97fe9b828ca372e74
thakuratul2 <pratapsinghatul111@gmail.com>
2023-08-09 20:04:40 +0530

    Shop Done

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -5,69 +5,79 @@ const Shop = () => {
   return (
     <>
         <div className="shop-section">
-            <div className="box1 box">
+            <div className="box">
             <div className="box-content">
             <h2>
-                Health & Personal Care
+                Clothes
             </h2>
             <div className="box-img1">
 
             </div>
-            </div>
             <p>See More</p>
             </div>
-            <div className="box2 box">
+            
+            </div>
+            <div className="box">
             <div className="box-content">
             <h2>
-                Electronics
+                Health & Personal Care
             </h2>
             <div className="box-img2">
 
             </div>
+            <p>See More</p>
             </div>
+            
             </div>
-            <div className="box3 box">
+            <div className="box">
             <div className="box-content">
             <h2>
-                Fashion
+               Furniture
             </h2>
             <div className="box-img3">
 
             </div>
+            <p>See More</p>
             </div>
+            
             </div>
-            <div className="box4 box">
+            <div className="box">
             <div className="box-content">
             <h2>
-            Home Appliances
+            Electronics
             </h2>
             <div className="box-img4">
 
             </div>
+            <p>See More</p>
             </div>
+            
             </div>
-            <div className="box1 box">
+            <div className="box">
             <div className="box-content">
             <h2>
-                Kitchen
+                Beauty Picks
             </h2>
             <div className="box-img5">
 
             </div>
-            </div>
             <p>See More</p>
             </div>
-            <div className="box2 box">
+            
+            </div>
+            <div className="box">
             <div className="box-content">
             <h2>
-                
+                New Arrival in Toys
             </h2>
             <div className="box-img6">
 
             </div>
+            <p>See More</p>
             </div>
+            
             </div>
-            <div className="box3 box">
+            <div className="box">
             <div className="box-content">
             <h2>
                 Beauty
@@ -75,17 +85,21 @@ const Shop = () => {
             <div className="box-img7">
 
             </div>
+            <p>See More</p>
             </div>
+            
             </div>
-            <div className="box4 box">
+            <div className="box">
             <div className="box-content">
             <h2>
-               
+               Discover Fashion Trends
             </h2>
             <div className="box-img8">
 
             </div>
+            <p>See More</p>
             </div>
+            
             </div>
         </div>
     </>
