fbf6b77a16bf2da6c231927f05f6c1b9bd68d9c7
thakuratul2 <pratapsinghatul111@gmail.com>
2023-08-09 20:23:52 +0530

    Clone Done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <>
+    <footer>
+    <div className="foot-panel1">
+        Back to Top
+    </div>
+    <div className="foot-panel2">
+        <ul>
+            <p>Get to Know Us</p>
+            <a href="#">Careers</a>
+            <a href="#">Blog</a>
+            <a href="#">About Amazon</a>
+            <a href="#">Investor Relations</a>
+            <a href="#">Amazon Devices</a>
+            <a href="#">Amazon Science</a>
+        </ul>
+        <ul>
+            <p>Get to Know Us</p>
+            <a href="#">Careers</a>
+            <a href="#">Blog</a>
+            <a href="#">About Amazon</a>
+            <a href="#">Investor Relations</a>
+            <a href="#">Amazon Devices</a>
+            <a href="#">Amazon Science</a>
+        </ul>
+        <ul>
+            <p>Get to Know Us</p>
+            <a href="#">Careers</a>
+            <a href="#">Blog</a>
+            <a href="#">About Amazon</a>
+            <a href="#">Investor Relations</a>
+            <a href="#">Amazon Devices</a>
+            <a href="#">Amazon Science</a>
+        </ul>
+        <ul>
+            <p>Get to Know Us</p>
+            <a href="#">Careers</a>
+            <a href="#">Blog</a>
+            <a href="#">About Amazon</a>
+            <a href="#">Investor Relations</a>
+            <a href="#">Amazon Devices</a>
+            <a href="#">Amazon Science</a>
+        </ul>
+    </div>
+    <div className="foot-panel3">
+        <div className="logo">
+
+        </div>
+        <div className="foot-panel4">
+            <div className="pages">
+                <a href="#">Conditions of Use</a>
+                <a href="#">Privacy Notice</a>
+                <a href="#">Your Ads Privacy Choices</a>
+            </div>
+            <div className="copyright">
+            © 1996-2023, Amazon.com, Inc. or its affiliates
+            </div>
+        </div>
+    </div>
+    </footer>
+    
+    </>
+  );
+}
+
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './Navbar';
 import Hero from './Hero';
 import Shop from './Shop';
+import Footer from './Footer';
 
 const Header = () => {
   return (
@@ -9,6 +10,7 @@ const Header = () => {
         <Navbar />
         <Hero />
         <Shop />
+        <Footer />
     </>
   );
 }
